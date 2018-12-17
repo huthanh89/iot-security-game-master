65be8836517d4685b600c308f8a67701aef6cc47
Thanh Huynh <hu.thanh89@yahoo.com>
2018-12-16 18:48:30 -0800

    formatting

--- game-server/html/instructor/components/setting.js
+++ game-server/html/instructor/components/setting.js
@@ -7,38 +7,36 @@ function Controller($scope, $uibModal){
   $scope.showSettings = function() {
     var template = $('.settings-modal-template').html();
     var modalInstance = $uibModal.open({
-        animation: true,
-        template:  template,
-        scope:    $scope,
-        controller: function($uibModalInstance) {
-            $ctrl = this;
-            $ctrl.enableInternet = $scope.internetEnabled;
-            $ctrl.ok = function() {
+      animation: true,
+      template:  template,
+      scope:    $scope,
+      controller: function($uibModalInstance) {
+        $ctrl = this;
+        $ctrl.enableInternet = $scope.internetEnabled;
+        $ctrl.ok = function() {
 
-              console.log('click');
+          $uibModalInstance.dismiss('ok');
+          
+          if ($scope.internetEnabled != $ctrl.enableInternet) {
+            ws.send(JSON.stringify({
+              type: 'internet',
+              enable: ($ctrl.enableInternet == true)
+            }));
+          }
+        };
 
-              $uibModalInstance.dismiss('ok');
+        $ctrl.cancel = function() {
+          $uibModalInstance.dismiss('cancel');
+        };
 
-              if ($scope.internetEnabled != $ctrl.enableInternet) {
-                  ws.send(JSON.stringify({
-                      type: 'internet',
-                      enable: ($ctrl.enableInternet == true)
-                  }));
-              }
-            };
-
-            $ctrl.cancel = function() {
-                $uibModalInstance.dismiss('cancel');
-            };
-
-            $ctrl.close = function() {
-                $uibModalInstance.close('saved');
-            }
-        },
-        controllerAs: 'ctrl',
-        windowClass: 'mission-modal-window',
-        size: 'md',
-        backdrop: false
+        $ctrl.close = function() {
+          $uibModalInstance.close('saved');
+        }
+      },
+      controllerAs: 'ctrl',
+      windowClass: 'mission-modal-window',
+      size: 'md',
+      backdrop: false
     });
 
     modalInstance.result.then(function(response) {}, function() {});
